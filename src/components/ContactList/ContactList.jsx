e108b2eeaa0231d7de64617e138e179693f55ff4
KateMassa <catherineorlova27@gmail.com>
2024-04-10 11:30:13 +0200

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,9 +2,10 @@ import { useSelector, useDispatch } from "react-redux";
 import Contact from "../Contact/Contact";
 import { deleteContact } from "../../redux/contactsSlice";
 import css from "./ContactList.module.css";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 const ContactList = () => {
-  const contacts = useSelector((state) => state.contacts.items);
+  const contacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
 
   const handleDelete = (id) => {
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -25,5 +25,5 @@ const contactsSlice = createSlice({
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
-export const selectContacts = (state) => state.contacts.items;
+export const selectFilteredContacts = (state) => state.contacts.items;
 export default contactsSlice.reducer;
